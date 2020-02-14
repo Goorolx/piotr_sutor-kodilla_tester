77f9ea2350a72b8e65b4e6fb3dbb59d3b08ab4ed
Goorolx <sutorpiotr@gmail.com>
2020-02-14 11:11:26 +0100

    correted naming convention

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -6,35 +6,35 @@ public class FirstClass {
         notebook.checkWeight();
         notebook.ourRecomendation();
 
-        Notebook old_notebook = new Notebook(1200,1500,2012);
-        old_notebook.checkPrice();
-        old_notebook.checkYear();
-        old_notebook.checkWeight();
-        old_notebook.ourRecomendation();
+        Notebook oldNotebook = new Notebook(1200,1500,2012);
+        oldNotebook.checkPrice();
+        oldNotebook.checkYear();
+        oldNotebook.checkWeight();
+        oldNotebook.ourRecomendation();
 
-        Notebook heavy_notebook = new Notebook(2000,2000,2018);
-        heavy_notebook.checkPrice();
-        heavy_notebook.checkYear();
-        heavy_notebook.checkWeight();
-        heavy_notebook.ourRecomendation();
+        Notebook heavyNotebook = new Notebook(2000,2000,2018);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkYear();
+        heavyNotebook.checkWeight();
+        heavyNotebook.ourRecomendation();
 
-        Notebook heavy_old_notebook = new Notebook(2000,350,2011);
-        heavy_old_notebook.checkPrice();
-        heavy_old_notebook.checkYear();
-        heavy_old_notebook.checkWeight();
-        heavy_old_notebook.ourRecomendation();
+        Notebook heavyOldNotebook = new Notebook(2000,350,2011);
+        heavyOldNotebook.checkPrice();
+        heavyOldNotebook.checkYear();
+        heavyOldNotebook.checkWeight();
+        heavyOldNotebook.ourRecomendation();
 
-        Notebook and_notebook = new Notebook(800,1099,2020);
-        and_notebook.checkPrice();
-        and_notebook.checkYear();
-        and_notebook.checkWeight();
-        and_notebook.ourRecomendation();
+        Notebook andNotebook = new Notebook(800,1099,2020);
+        andNotebook.checkPrice();
+        andNotebook.checkYear();
+        andNotebook.checkWeight();
+        andNotebook.ourRecomendation();
 
-        Notebook int_notebook = new Notebook(5400,1999,2007);
-        int_notebook.checkPrice();
-        int_notebook.checkYear();
-        int_notebook.checkWeight();
-        int_notebook.ourRecomendation();
+        Notebook intNotebook = new Notebook(5400,1999,2007);
+        intNotebook.checkPrice();
+        intNotebook.checkYear();
+        intNotebook.checkWeight();
+        intNotebook.ourRecomendation();
 
     }
 }
