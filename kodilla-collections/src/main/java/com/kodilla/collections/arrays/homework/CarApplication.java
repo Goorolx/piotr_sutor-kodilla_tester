145bd663b2f83c39e83ba0263b4bc61757381fb7
Goorolx <sutorpiotr@gmail.com>
2020-03-05 06:14:26 +0100

    Aplikacja działa, domknąłem pętle for

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarApplication.java
@@ -10,32 +10,33 @@ import java.util.Random;
 public class CarApplication {
     public static void main(String[] args) {
         Random random = new Random();
-        Car[] cars = new Car[random.nextInt(15)+1];
-        for (int i = 0 ; i < cars.length ; i++){
-            cars[i]=drawCar();
-        for (int n=0;n <cars.length; n++){
-            CarUtils.describeCar(cars[n]);
+        Car[] cars = new Car[random.nextInt(15) + 1];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = drawCar();
         }
+//        for (int n = 0; n < cars.length; n++) {
+//            CarUtils.describeCar(cars[n]);}
 
-//        for (Car car : cars)
-//            CarUtils.describeCar(car);
-        }
+       for (Car car : cars)
+            CarUtils.describeCar(car);
     }
 
-    public static Car drawCar(){
+
+    public static Car drawCar() {
         Random random = new Random();
         int drawNewCar = random.nextInt(3);
-        int a = random.nextInt(10)+1;
-        int b = random.nextInt(10)+1;
-        int c = random.nextInt(10)+1;
+        int a = random.nextInt(10) + 1;
+        int b = random.nextInt(10) + 1;
+        int c = random.nextInt(10) + 1;
 
         if (drawNewCar == 0)
-            return new Ford(a,b,c);
+            return new Ford(a, b, c);
         else if (drawNewCar == 1)
-            return new Opel(a,b,c);
+            return new Opel(a, b, c);
         else
-            return new Star(a,b,c);
+            return new Star(a, b, c);
 
     }
-
 }
+
+
