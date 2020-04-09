51b6ab2fd2410702871824c51dbd5ceea3d98629
Goorolx <sutorpiotr@gmail.com>
2020-04-09 17:41:00 +0200

    Bankomat poprawiony

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -1,109 +1,137 @@
 package com.kodilla.bank.homework;
 
-import static java.lang.Double.NaN;
-
 public class Bank {
     public String name;
-    public CashMachine cm1;
-    public CashMachine cm2;
+    public CashMachine[] cm;
+    public int size;
+    //public CashMachine cm2;
 
-    public Bank (String name) {
+    public Bank(String name) {
         this.name = name;
-        this.cm1 = new CashMachine();
-        this.cm2 = new CashMachine();
-    }
-    public void addCm1Transaction(int value){
-        if (value != 0) {
-        this.cm1.addTransaction(value);
-    }}
-    public void addCm2Transaction(int value){
-        if (value!= 0) {
-            this.cm2.addTransaction(value);
-        }}
+        this.cm = new CashMachine[0];
 
-        public double getInCashCm1() {
-        int[] cm1Transactions = cm1.getTransactions();
-        double sumInCm1 = 0;
-        for (int cm1t : cm1Transactions) {
-            if (cm1t > 0)
-                sumInCm1 = sumInCm1 + cm1t; }
-        return sumInCm1;
     }
-    public double getOutCashCm1() {
-        int[] cm1Transactions = cm1.getTransactions();
-        double sumOutCm1 = 0;
-        for (int cm1t : cm1Transactions) {
-            if (cm1t < 0)
-                sumOutCm1 = sumOutCm1 + cm1t;
+
+    public void addCashMachine(CashMachine cashMachine) {
+        this.size++;
+        CashMachine[] tempTab = new CashMachine[this.size];
+        System.arraycopy(cm, 0, tempTab, 0, cm.length);
+        tempTab[this.size - 1] = cashMachine;
+        this.cm = tempTab;
+    }
+
+    public void addCmTransaction(int cmIndex, int value) {
+        if (value != 0) {
+            this.cm[cmIndex].addTransaction(value);
         }
-        return sumOutCm1;
     }
-    public int getInCountCm1() {
-        int[] cm1Transactions = cm1.getTransactions();
+
+    public double getInCashCm(CashMachine cm) {
+        int[] cmTransactions = cm.getTransactions();
+        double sumInCm = 0;
+        for (int cm1t : cmTransactions) {
+            if (cm1t > 0)
+                sumInCm = sumInCm + cm1t;
+        }
+        return sumInCm;
+    }
+
+    public int getOutCashCm(CashMachine cm) {
+        int[] cmTransactions = cm.getTransactions();
+        int sumOutCm = 0;
+        for (int cm1t : cmTransactions) {
+            if (cm1t < 0)
+                sumOutCm = sumOutCm + cm1t;
+        }
+        return sumOutCm;
+    }
+
+    public int getInCountCm(CashMachine cm) {
+        int[] cm1Transactions = cm.getTransactions();
         int countInCm1 = 0;
         for (int cm1t : cm1Transactions) {
             if (cm1t > 0)
-                 countInCm1++; }
+                countInCm1++;
+        }
         return countInCm1;
     }
-    public int getOutCountCm1() {
-        return cm1.getNumberOfTransactions() - getInCountCm1();
-    }
-    public double getInCashCm2() {
-        int[] cm2Transactions = cm2.getTransactions();
-        double sumInCm2 = 0;
-        for (int cm2t : cm2Transactions) {
-            if (cm2t > 0)
-                sumInCm2 = sumInCm2 + cm2t;
-                    }
-        return sumInCm2;
-    }
-    public double getOutCashCm2() {
-        int[] cm2Transactions = cm2.getTransactions();
-        double sumCm2 = 0;
-        for (int cm2t : cm2Transactions) {
-            if (cm2t < 0)
-                sumCm2 = sumCm2 + cm2t;
-                    }return sumCm2;
-    }
-    public int getOutCountCm2() {
-        return cm2.getNumberOfTransactions() - getInCountCm2();
+
+    public int getOutCountCm(CashMachine cm) {
+        int[] cm1Transactions = cm.getTransactions();
+        int countOutCm = 0;
+        for (int cm1t : cm1Transactions) {
+            if (cm1t < 0)
+                countOutCm++;
+        }
+        return countOutCm;
     }
 
-    public int getInCountCm2() {
-        int[] cm2Transactions = cm2.getTransactions();
-        int countInCm2 = 0;
-        for (int cm2t : cm2Transactions) {
-            if (cm2t > 0)
-                countInCm2++; }
-        return countInCm2;
-    }
-    public double getInAvg (){
-        double inAvg = 0.0;
-        inAvg = ((getInCashCm1() / getInCountCm1()) + (getInCashCm2() / getInCountCm2()))/2;
-        if (Double.isNaN(inAvg))
-            return 0;
-        else
-            return inAvg;
-                            }
+    public double getInAvg() {
+        double inCash = 0.0;
+        double inCount = 0.0;
+        Bank bank = new Bank("pko");
+        for (CashMachine cm : cm
+        ) {
+            inCount = inCount + bank.getInCountCm(cm);
+        }
 
-    public double getOutAvg () {
-        double outAvg =0.0;
-        outAvg = (getOutCashCm1()/getOutCountCm1() + getOutCashCm2()/getOutCountCm2())/2;
-        if ((Double.isNaN(outAvg)))
-            return 0;
+        for (CashMachine cmCash : cm
+        ) {
+            inCash = inCash + bank.getInCashCm(cmCash);
+        }
+        if (inCount != 0)
+            return inCash / inCount;
         else
-            return outAvg;
+            return 0.0;
+    }
+
+    public double getOutAvg() {
+        double outCash = 0.0;
+        double outCount = 0.0;
+        Bank bank = new Bank("pko");
+        for (CashMachine cm : cm
+        ) {
+            outCount = outCount + bank.getOutCountCm(cm);
+        }
+
+        for (CashMachine cmCash : cm
+        ) {
+            outCash = outCash + bank.getOutCashCm(cmCash);
+        }
+        if (outCount != 0)
+            return outCash / outCount;
+        else
+            return 0.0;
+    }
+
+    public double getTotalSaldo() {
+        double totalSaldo = 0.0;
+        for (CashMachine cmSaldo : cm) {
+            totalSaldo = totalSaldo + cmSaldo.getSaldo();
+        }
+        return totalSaldo;
+    }
+
+    public double getInAll() {
+        Bank bank = new Bank("pko");
+        double inAll = 0.0;
+        for (CashMachine cm : cm
+        ) {
+            inAll = inAll + bank.getInCountCm(cm);
+        }
+        return inAll;
+    }
+
+    public double getOutAll() {
+        Bank bank = new Bank("pko");
+        double outAll = 0.0;
+        for (CashMachine cm : cm
+        ) {
+            outAll = outAll + bank.getOutCountCm(cm);
+        }
+        return outAll;
+    }
 }
-
-    public double getTotalSaldo (){
-        return cm1.getSaldo()+cm2.getSaldo();
-    }
-
-    public int getInAll () {return (getInCountCm1()+getInCountCm2());}
-
-    public int getOutAll () {return (getOutCountCm1()+getOutCountCm2());}
-   }
 
 
 
--- kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
@@ -6,91 +6,128 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class BankTestSuite {
+
+    @Test
+    public void shouldAdd3CashMachines() {
+        Bank bank = new Bank("alior");
+        bank.addCashMachine(new CashMachine());
+        bank.addCashMachine(new CashMachine());
+        bank.addCashMachine(new CashMachine());
+
+        int cmNumber = bank.cm.length;
+
+        assertEquals(3, cmNumber);
+    }
+
     @Test
     public void shouldReturnZeroSaldo() {
         Bank bank1 = new Bank("pekao");
-            bank1.addCm2Transaction(0);
-            bank1.addCm1Transaction(0);
-            bank1.addCm1Transaction(0);
+        bank1.addCashMachine(new CashMachine());
+        bank1.addCashMachine(new CashMachine());
+        bank1.addCmTransaction(0, 0);
+        bank1.addCmTransaction(1, 0);
 
-            double bank = bank1.getTotalSaldo();
-        assertEquals(0,bank);
+        double bank = bank1.getTotalSaldo();
+        assertEquals(0, bank);
     }
+
     @Test
     public void shouldCalculateCorrectSaldo() {
         Bank bank = new Bank("Pko");
-        bank.addCm1Transaction(200);
-        bank.addCm1Transaction(200);
-        bank.addCm1Transaction(-400);
-        bank.addCm2Transaction(400);
-        bank.addCm2Transaction(400);
-        bank.addCm2Transaction(-100);
-        bank.addCm2Transaction(0);
+        bank.addCashMachine(new CashMachine());
+        bank.addCashMachine(new CashMachine());
+        bank.addCmTransaction(0, 200);
+        bank.addCmTransaction(0, 200);
+        bank.addCmTransaction(0, -400);
+        bank.addCmTransaction(0, 400);
+        bank.addCmTransaction(1, 400);
+        bank.addCmTransaction(1, -100);
+        bank.addCmTransaction(1, 0);
 
         double saldo = bank.getTotalSaldo();
         assertEquals(700, saldo, 0.01);
     }
+
     @Test
-    public void shouldReturnZeroCash(){
+    public void shouldReturnZeroCash() {
         Bank bank1 = new Bank("pekao");
-        bank1.addCm2Transaction(0);
-        bank1.addCm1Transaction(0);
-        bank1.addCm1Transaction(0);
-    assertEquals(0,bank1.getOutAll());
-    assertEquals(0,bank1.getOutAll());
+        bank1.addCashMachine(new CashMachine());
+        bank1.addCashMachine(new CashMachine());
+        bank1.addCmTransaction(1, 0);
+        bank1.addCmTransaction(0, 0);
+        bank1.addCmTransaction(0, 0);
+        assertEquals(0, bank1.getOutAll());
     }
-    @Test
-    public void shouldReturnCorrectInCashCount(){
-        Bank bank = new Bank("Pko");
-        bank.addCm1Transaction(200);
-        bank.addCm2Transaction(400);
-        bank.addCm2Transaction(-400);
-        bank.addCm1Transaction(400);
-        bank.addCm2Transaction(00);
 
-        assertEquals(3,bank.getInAll());
-    }
     @Test
-    public void shouldReturnCorrectOutCashCount(){
+    public void shouldReturnCorrectInCashCount() {
         Bank bank = new Bank("Pko");
-        bank.addCm1Transaction(-200);
-        bank.addCm2Transaction(400);
-        bank.addCm2Transaction(-400);
-        bank.addCm1Transaction(400);
-        bank.addCm2Transaction(00);
+        bank.addCashMachine(new CashMachine());
+        bank.addCashMachine(new CashMachine());
+        bank.addCmTransaction(0, 200);
+        bank.addCmTransaction(1, 400);
+        bank.addCmTransaction(1, -400);
+        bank.addCmTransaction(0, 400);
+        bank.addCmTransaction(1, 00);
 
-        assertEquals(2,bank.getOutAll());
+        assertEquals(3, bank.getInAll());
     }
+
+    @Test
+    public void shouldReturnCorrectOutCashCount() {
+        Bank bank = new Bank("Pko");
+        bank.addCashMachine(new CashMachine());
+        bank.addCashMachine(new CashMachine());
+        bank.addCmTransaction(0, -200);
+        bank.addCmTransaction(1, 400);
+        bank.addCmTransaction(1, -400);
+        bank.addCmTransaction(0, -400);
+        bank.addCmTransaction(1, 00);
+
+        assertEquals(3, bank.getOutAll());
+    }
+
     @Test
     public void shouldReturnZeroAvg() {
         Bank bank1 = new Bank("pekao");
-        bank1.addCm2Transaction(0);
-        bank1.addCm1Transaction(0);
-        bank1.addCm1Transaction(0);
+        bank1.addCashMachine(new CashMachine());
+        bank1.addCashMachine(new CashMachine());
+        bank1.addCmTransaction(0, 0);
+        bank1.addCmTransaction(0, 0);
+        bank1.addCmTransaction(0, 0);
         assertEquals(0, bank1.getOutAvg());
         assertEquals(0, bank1.getInAvg());
     }
-       @Test
-        public void shouldReturnAvgInCash() {
+
+    @Test
+    public void shouldReturnAvgInCash() {
         Bank bank = new Bank("Pko");
-        bank.addCm1Transaction(100);
-        bank.addCm2Transaction(50);
-        bank.addCm1Transaction(200);
-        bank.addCm2Transaction(100);
-        bank.addCm2Transaction(-400);
-        bank.addCm1Transaction(-450);
-        bank.addCm2Transaction(00);
+        bank.addCashMachine(new CashMachine());
+        bank.addCashMachine(new CashMachine());
+        bank.addCashMachine(new CashMachine());
+        bank.addCashMachine(new CashMachine());
+        bank.addCmTransaction(1, 100);
+        bank.addCmTransaction(2, 50);
+        bank.addCmTransaction(3, 200);
+        bank.addCmTransaction(0, 100);
+        bank.addCmTransaction(3, -400);
+        bank.addCmTransaction(2, -450);
+        bank.addCmTransaction(0, 00);
 
-        assertEquals(112.5,bank.getInAvg());
-        }
-        @Test
-    public void shouldReturnAvgOutCash(){
-            Bank bank = new Bank("Pko");
-            bank.addCm1Transaction(-200);
-            bank.addCm2Transaction(-400);
-            bank.addCm2Transaction(100);
-            bank.addCm2Transaction(00);
+        assertEquals(112.50, bank.getInAvg());
+    }
 
-            assertEquals(-300,bank.getOutAvg());
-        }
+    @Test
+    public void shouldReturnAvgOutCash() {
+        Bank bank = new Bank("Pko");
+        bank.addCashMachine(new CashMachine());
+        bank.addCashMachine(new CashMachine());
+        bank.addCmTransaction(0, 100);
+        bank.addCmTransaction(0, -1000);
+        bank.addCmTransaction(1, -500);
+        bank.addCmTransaction(1, 100);
+        bank.addCmTransaction(1, 00);
+
+        assertEquals(-750, bank.getOutAvg());
+    }
 }
